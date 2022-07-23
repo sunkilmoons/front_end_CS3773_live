f6ad2364fdb171c764b73285182b79e9e949385b
kej-ngwpo-ine-FW <31877057+kej-ngwpo-ine-FW@users.noreply.github.com>
2022-07-23 14:54:54 -0500

    Cart Page Update
    Overhaul of Cart Page:
    
    -Added Cart prices to bottom of page
    
    -Added CartPage.css, to style the cart page

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,15 +1,37 @@
+import { useState } from 'react'
+import { useDatabase } from '../hooks/useDatabase'
+import { Cart } from '../model'
+import '../styles/CartPage.css'
+
 export const CartPage = () => {
+  const [state, dispatch] = useDatabase()
+
   return (
-    <div className="Cart-Page" style={{ display: 'flex' }}>
+    <div className="Cart">
+      <div className='cart-header'>
       <h1
         style={{
           flex: 1,
+          color: 'Black',
           textAlign: 'center',
-          alignSelf: 'center',
-        }}
-      >
-        Shopping Cart
+        }}>
+        Your Shopping Cart:
       </h1>
+      </div>
+      <div className='TotalBar'>
+      <p
+        style={{color: 'Black', padding: 20, alignSelf: 'left'}}>
+          Sub: {Cart.getSubtotal(state.loggedInUser?.cart)}
+      </p>
+      <p
+      style={{color: 'Black', padding: 20, alignSelf: 'center'}}>
+        Tax: {Cart.getSalesTax(state.loggedInUser?.cart)}
+      </p>
+      <p
+      style={{color: 'Black', padding: 20, alignSelf: 'right'}}>
+        Tot: {Cart.getTotal(state.loggedInUser?.cart)}</p>
     </div>
+    </div>
+  
   )
 }
