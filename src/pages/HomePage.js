5a54790843fec8255480b22ddc80d03311162afc
Eduardo-Rosales-Tx <eduardo.rosales@live.com>
2022-07-26 20:12:23 -0500

    sort_option testing

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,32 +1,50 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import pic from '../assets/plus_sign.png'
 import { useDatabase, useLoginWithTestUser } from '../hooks'
 
 export const HomePage = () => {
   const [state, dispatch] = useDatabase()
 
-  const [searchText, setSearchText] = useState('')
+    const [searchText, setSearchText] = useState('')
+
+    const [item_choice, setItem_choice] = useState([])
+    const [sortType, setSortType] = useState('price')
 
   // login with test user so we have a cart to work with.
   // until we get the account page setup.
   useLoginWithTestUser()
 
-  const handleClick = (item) => {
-    // add item to cart...
-    dispatch({ type: 'add item', payload: { item } })
+    const handleClick = (item) => {
+        // add item to cart...
+        dispatch({ type: 'add item', payload: { item } })
 
-    alert(
-      `You clicked item: ${JSON.stringify(
-        item,
-        null,
-        2
-      )}\n\nThe previous cart looked like ${JSON.stringify(
-        state.loggedInUser?.cart?.items,
-        null,
-        4
-      )}`
-    )
-  }
+        alert(
+            `You clicked item: ${JSON.stringify(
+                item,
+                null,
+                2
+            )}\n\nThe previous cart looked like ${JSON.stringify(
+                state.loggedInUser?.cart?.items,
+                null,
+                4
+            )}`
+        )
+    }
+
+    /*useEffect(() => {
+        const sortArray = type => {
+            const types = {
+                price: 'price',
+                availability: 'availability',
+            };
+            const sortProperty = types[type]
+            const sorted = [...items].sort((a, b) => b[sortProperty] - a[sortproperty])
+            setItem_choice(sorted)
+        };
+
+        sortArray(sortType)
+
+    }, [sortType]);*/
 
   return (
     <div
@@ -64,7 +82,12 @@ export const HomePage = () => {
         value={searchText}
         onChange={(event) => setSearchText(event.target.value)}
         placeholder="search"
-      />
+          />
+          <select onChange={(event) => setSortType(event.target.value)}>
+              <option>Any Item</option>
+              <option value="price">Price</option>
+              <option value="availability">Availability</option>
+          </select>
       <ul
         style={{
           flex: 0,
