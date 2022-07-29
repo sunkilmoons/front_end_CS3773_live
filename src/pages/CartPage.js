adc3a3556e96def79b3626151b73eda49b83d40f
kej-ngwpo-ine-FW <31877057+kej-ngwpo-ine-FW@users.noreply.github.com>
2022-07-29 10:42:35 -0500

    Cart Update
    1-Changed header so the cart refers to the list as {username} Shopping cart, rather than just "Your Shopping cart
    
    2-Created a list that shows each item in the users cart, with an added remove button to remove that particular item should it be needed
    
    3-added a "Coupon" function that simply holds a value for a coupon code
    
    4-added a bar beneath the pricing bar, this bar contains:
      4.1-"Clear Cart" button, that removes all items from the user's cart
      4.2- "Checkout" button, which takes the items away from stock and empties the cart
      4.3- An input field with an "Apply" button, the input field will have a placeholder of "Enter Coupon Code", which will then use the Coupon function to store the value of written input, with the apply button using the stored value for input in the "apply coupon" case of useDatabase

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -3,9 +3,21 @@ import { useDatabase } from '../hooks/useDatabase'
 import { Cart } from '../model'
 import '../styles/CartPage.css'
 
+function Coupon(defaultValue) {
+  const [code, setCode] = useState(defaultValue);
+  function onChange(e) {
+    setCode(e.target.value);
+  }
+  return {
+    code,
+    onChange,
+  };
+}
+
+
 export const CartPage = () => {
   const [state, dispatch] = useDatabase()
-
+  const coupon = Coupon();
   return (
     <div className="Cart">
       <div className='cart-header'>
@@ -15,8 +27,43 @@ export const CartPage = () => {
           color: 'Black',
           textAlign: 'center',
         }}>
-        Your Shopping Cart:
+        {state.loggedInUser?.username} Shopping Cart:
       </h1>
+      </div>
+      <div className='cart-list'>
+      <p
+      style={{
+        flex: 1,
+        color: 'Black',
+        textAlign: 'center',
+      }}>
+        {state.loggedInUser?.cart.items.map((item) => 
+        (
+          <div className='itemListing' key={item.index}>
+            <p>Item:  {item.name}  Cost: {item.price}</p>
+            <button onClick={() =>
+            dispatch({
+              type: 'remove item',
+              payload: {
+                item: {
+                  name: item.name,
+                  description: item.description,
+                  price: item.price,
+                  stockCount: item.stockCount,
+                },
+              },
+            })}
+            style={{
+              flex: 1,
+              color: 'Black',
+              textAlign: 'center',
+            }}>
+              remove
+            </button>
+          </div>
+        )
+        )}
+      </p>
       </div>
       <div className='TotalBar'>
       <p
@@ -31,6 +78,28 @@ export const CartPage = () => {
       style={{color: 'Black', padding: 20, alignSelf: 'right'}}>
         Tot: {Cart.getTotal(state.loggedInUser?.cart)}</p>
     </div>
+    <div className='ButtonBar'>
+    <button onClick={() => dispatch({ type: 'clear cart' })}
+    style={{color: 'Black', padding: 17, alignSelf: 'left'}}>Clear Cart</button>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <button onClick={() => dispatch({ type: 'checkout' })}
+    style={{color: 'Black', padding: 17, alignSelf: 'center'}}>Checkout</button>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <div class="Search">  
+    <input input {...coupon} placeholder="Enter Coupon Code"/>
+    <button onClick={() =>
+            dispatch({
+              type: 'apply coupon',
+              payload: {
+                couponCode: coupon.code,
+              },
+            })}>Apply</button> 
+    </div>
+
+    </div>
+
     </div>
   
   )
