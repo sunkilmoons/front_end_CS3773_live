e8057b9cc9a4a80e151e5d9c101d78085d7ad855
sunkilmoons <bcassidy50@gmail.com>
2022-07-31 21:09:21 -0500

    Change nav bar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,44 +1,44 @@
-import home from '../assets/home.svg'
 import Search from './search'
-import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { AiOutlineShoppingCart, AiFillHome } from 'react-icons/ai'
+import { BsFillPersonFill } from 'react-icons/bs'
 import person from '../assets/person.svg'
 import { Link } from 'react-router-dom'
 import '../styles/NavBar.css'
-var size='50px'
 
 export const NavBar = () => {
   return (
     <nav className="NavBar">
-      
-      <Link to="/" style={{ height: '100%',fontSize:'48px',font:'Times New Roman'}}>
-        <img src={home} alt={home} style={{ height: '100%',
-          // height:size,
-          // width:size,
-          backgroundColor: 'white',
-          borderRadius: '10px' }} />
-          
+      <Link
+        to="/"
+        style={{ height: '100%', fontSize: '48px', font: 'Times New Roman' }}
+      >
+        <AiFillHome
+          size={40}
+          className="hover-item"
+          style={{ color: 'black' }}
+        />
       </Link>
-
-      <div>
+      <div style={{ display: 'flex', flexDirection: 'row', gap: '4rem' }}>
         <Link to="/cart">
-          <AiOutlineShoppingCart size={40} style={{ cursor: 'pointer',
-            color: 'black',
-            backgroundColor:'white',
-            borderRadius:'10px'}} />
+          <AiOutlineShoppingCart
+            size={40}
+            className="hover-item"
+            style={{
+              color: 'black',
+            }}
+          />
         </Link>
-      </div>
-      <div class="dropdown-menu">
+        <div class="dropdown-menu">
           <Link to="/account">
-            <img src={person} alt={person} style={
-              { cursor: 'pointer',
-                backgroundColor: 'white',
-                borderRadius: '20px'}} />
-                
-
+            <BsFillPersonFill
+              className="hover-item"
+              size={40}
+              style={{ color: 'black ' }}
+            />
           </Link>
-          <Search/>
+          <Search />
+        </div>
       </div>
-     
     </nav>
   )
 }
