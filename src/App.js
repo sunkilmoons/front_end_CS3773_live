e912d4bbc614a235b8766590c45044e5e9f62d43
sunkilmoons <bcassidy50@gmail.com>
2022-07-31 21:54:07 -0500

    add db example link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import './App.css'
 import { NavBar } from './components/NavBar'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { HomePage, CartPage, AccountPage, RegistrationPage} from './pages/'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from 'react-router-dom'
+import { HomePage, CartPage, AccountPage, RegistrationPage } from './pages/'
 import { DBProvider } from './components'
 import { DatabaseExamplePage } from './pages/DatabaseExamplePage'
+import { FiChevronRight } from 'react-icons/fi'
+import { useEffect, useState } from 'react'
 
+const Pages = () => {
+  const location = useLocation()
+
+  const [showDbLink, setShowDbLink] = useState(true)
+  useEffect(() => {
+    setShowDbLink(location.pathname === '/db-example')
+  }, [location.pathname, setShowDbLink])
+
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="/db-example" element={<DatabaseExamplePage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+      </Routes>
+      <Link
+        to="db-example"
+        className="DB-Example-Link"
+        style={{
+          display: showDbLink ? 'none' : 'flex',
+        }}
+      >
+        <p>Database Example</p>
+        <FiChevronRight size={16} />
+      </Link>
+    </>
+  )
+}
 
 function App() {
   return (
     <DBProvider>
-      <Router style={{ width: '100vw', height: '100vh' }}>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/account" element={<AccountPage />} />
-          <Route path="/db-example" element={<DatabaseExamplePage />} />
-          <Route path="/register" element={<RegistrationPage />} />
-        </Routes>
+      <Router>
+        <Pages />
       </Router>
     </DBProvider>
   )
